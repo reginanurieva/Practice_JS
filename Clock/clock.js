51e463f47dce3bca858278bdacb87e9934c8e786
Regina Nurieva <regginurieva@gmail.com>
2018-10-21 01:27:24 -0700

    add initial files

---
+++ Clock/clock.js
@@ -0,0 +1,27 @@
+
+function setDate() {
+  const now = new Date();
+
+
+
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const secondHand = document.querySelector('.second-hand');
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+
+  const mins = now.getMinutes();
+  const hour = now.getMinutes();
+  const hourHand = document.querySelector('.hour-hand');
+  const hourDegrees = ((mins / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  const minsHand = document.querySelector('.min-hand');
+  const minsDegrees = ((mins / 60) * 360) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+
+console.log(seconds);
+}
+setInterval(setDate, 1000);
