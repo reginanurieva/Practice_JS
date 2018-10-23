824a646b3c6960c4d9d71e298bb117495f938cd2
Regina Nurieva <regina@>
2018-10-23 09:35:17 -0700

    add jquery

--- Clock/clock.js
+++ Clock/clock.js
@@ -1,27 +1,24 @@
+$(document).ready(function() {
+  
+  function setDate() {
+    const now = new Date();
 
-function setDate() {
-  const now = new Date();
+    const seconds = now.getSeconds();
+    const mins = now.getMinutes();
+    const hour = now.getHours();
 
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const secondHand = document.querySelector('.second-hand');
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
+    const hourHand = document.querySelector('.hour-hand');
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
-
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  const secondHand = document.querySelector('.second-hand');
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
-
-  const mins = now.getMinutes();
-  const hour = now.getMinutes();
-  const hourHand = document.querySelector('.hour-hand');
-  const hourDegrees = ((mins / 12) * 360) + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-
-  const minsHand = document.querySelector('.min-hand');
-  const minsDegrees = ((mins / 60) * 360) + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-
-
-console.log(seconds);
-}
-setInterval(setDate, 1000);
+    const minsHand = document.querySelector('.min-hand');
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  }
+  setDate();
+  setInterval(setDate, 1000);
+})
